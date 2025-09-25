cbe51e2e8db1181755f6ecfac76be214b414309b
Zeeshan Haider <zeeshanhaider538@gmail.com>
2025-09-25 12:51:35 +0500

    Update the Navbar

--- memoai/src/app/notes/Navbar.tsx
+++ memoai/src/app/notes/Navbar.tsx
@@ -1,29 +1,42 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/assets/logo.png";
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
+import { useState } from "react";
+import AddNoteDialoge from "@/components/AddNotesDialog";
 export default function Navbar() {
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
   return (
-    <div className="p-4 shadow">
-      <div className="max-w-7xl m-auto flex justify-between items-center flex-wrap gap-3">
-        <Link href="/notes" className="flex items-center gap-1">
-          <Image src={logo} alt="Memo AI logo" width={40} height={40} />
-          <span className="font-bold pt-2.5">Memo AI</span>
-        </Link>
-        <div className="flex items-center gap-4">
-          <UserButton
-            appearance={{
-              elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
-            }}
-          />
-          <Button>
-            <Plus size={20} className="mr-2" />
-            Add Note
-          </Button>
+    <>
+      <div className="p-4 shadow">
+        <div className="max-w-7xl m-auto flex justify-between items-center flex-wrap gap-3">
+          <Link href="/notes" className="flex items-center gap-1">
+            <Image
+              src={logo}
+              alt="Memo AI logo"
+              width={40}
+              height={40}
+              className="w-auto h-auto"
+            />
+            <span className="font-bold pt-2.5">Memo AI</span>
+          </Link>
+          <div className="flex items-center gap-4">
+            <UserButton
+              appearance={{
+                elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
+              }}
+            />
+            <Button onClick={() => setShowAddNoteDialog(true)}>
+              <Plus size={20} className="mr-2" />
+              Add Note
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+      <AddNoteDialoge open={showAddNoteDialog} setOpen={setShowAddNoteDialog} />
+    </>
   );
 }
