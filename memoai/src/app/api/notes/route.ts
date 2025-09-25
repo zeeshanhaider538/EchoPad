db4b1a723a99d589b0e5906dcef605800d3c2856
Zeeshan Haider <zeeshanhaider538@gmail.com>
2025-09-25 12:50:52 +0500

    added put request for the notes

--- memoai/src/app/api/notes/route.ts
+++ memoai/src/app/api/notes/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/db";
+import { getNoteById, updatedNotes } from "@/db/repositories/notes";
 import { notes } from "@/db/schema";
-import { createNoteSchema } from "@/lib/validation/notes";
+import { createNoteSchema, updatedNoteSchema } from "@/lib/validation/notes";
 import { auth } from "@clerk/nextjs/server";
 
 export async function POST(req: Request) {
@@ -25,6 +26,35 @@ export async function POST(req: Request) {
       })
       .returning();
     return Response.json(note[0], { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
+
+export async function PUT(req: Request) {
+  try {
+    const body = await req.json();
+    const parseResult = updatedNoteSchema.safeParse(body);
+    if (!parseResult.success) {
+      console.error(parseResult.error);
+      return Response.json({ error: "Invalid input" }, { status: 400 });
+    }
+    const { id, title, content } = parseResult.data;
+    const { userId } = await auth();
+
+    const note = await getNoteById(id);
+    if (!note) {
+      return Response.json({ error: "Note not found" }, { status: 404 });
+    }
+    if (!userId || userId !== note.user_id) {
+      return Response.json({ error: "Unauthorized " }, { status: 401 });
+    }
+    const updatedNote = await updatedNotes(note.id, {
+      title,
+      content,
+    });
+    return Response.json({ note: updatedNote }, { status: 200 });
   } catch (error) {
     console.error(error);
     return Response.json({ error: "Internal server error" }, { status: 500 });
