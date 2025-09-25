c681c56348a16fb293b800b8f8b619a3fc5c672e
Zeeshan Haider <zeeshanhaider538@gmail.com>
2025-09-25 12:52:23 +0500

    Update the Notes page

--- memoai/src/app/notes/page.tsx
+++ memoai/src/app/notes/page.tsx
@@ -1,15 +1,27 @@
+import Note from "@/components/ui/Note";
+import { getUserNotes } from "@/db/repositories/notes";
+import { auth } from "@clerk/nextjs/server";
 import { Metadata } from "next";
 
 // import React from 'react'
 export const metadata: Metadata = {
   title: "MemoAI - Notes",
 };
-const notes = () => {
+export default async function NotesPage() {
+  const { userId } = await auth();
+  if (!userId) throw Error("UserId Undefined");
+  const allNotes = await getUserNotes(userId);
+  console.log(allNotes);
   return (
-    <>
-      <h1>notes</h1>
-    </>
+    <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
+      {allNotes.map((note) => (
+        <Note note={note} key={note.id} />
+      ))}
+      {allNotes.length === 0 && (
+        <div className="col-span-full text-center">
+          {" You don't have any notes yet. You should create one?"}
+        </div>
+      )}
+    </div>
   );
-};
-
-export default notes;
+}
