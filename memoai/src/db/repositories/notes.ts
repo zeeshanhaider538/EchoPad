c08d47c50a513e3f2ea45487bf30e219e9a708bf
Zeeshan Haider <zeeshanhaider538@gmail.com>
2025-09-25 12:56:59 +0500

    added function of getNodeById and UpdatedNotes

---
+++ memoai/src/db/repositories/notes.ts
@@ -0,0 +1,34 @@
+import { eq } from "drizzle-orm";
+import { db } from "..";
+import { notes } from "../schema";
+
+export async function getUserNotes(userId: string) {
+  const result = await db.select().from(notes).where(eq(notes.user_id, userId));
+  return result;
+}
+
+export async function getNoteById(noteId: number) {
+  const result = await db
+    .select()
+    .from(notes)
+    .where(eq(notes.id, noteId))
+    .limit(1);
+
+  return result[0] ?? null;
+}
+
+type UpdateNoteProp = {
+  title?: string; // optional but never null
+  content?: string | null; // optional and can be null
+};
+export async function updatedNotes(id: number, data: UpdateNoteProp) {
+  const [updatedNote] = await db
+    .update(notes)
+    .set({
+      ...data,
+      updated_at: new Date(),
+    })
+    .where(eq(notes.id, id))
+    .returning();
+  return updatedNote;
+}
