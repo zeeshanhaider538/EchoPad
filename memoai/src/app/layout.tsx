e31ad0bea2a1173bf17a829210af97288cd9c0a4
Zeeshan Haider <zeeshanhaider538@gmail.com>
2025-09-25 09:28:01 +0500

    cleaning the layout page

--- memoai/src/app/layout.tsx
+++ memoai/src/app/layout.tsx
@@ -32,31 +32,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    // <html lang="en">
-    //   <body
-    //     className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-    //   >
-    //     {children}
-    //   </body>
-    // </html>
     <ClerkProvider>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {/* <header className="flex justify-end items-center p-4 gap-4 h-16 bg-gray-400">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header> */}
           {children}
         </body>
       </html>
